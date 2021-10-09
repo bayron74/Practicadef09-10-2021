7053c306a99f6abf12856f8ea77a0d1a491146cd
bayron74 <88299465+bayron74@users.noreply.github.com>
2021-10-09 10:48:18 -1200

    tercer commit

--- Ejercicios 09-10 2021.py
+++ Ejercicios 09-10 2021.py
@@ -3,19 +3,27 @@ def suma(n1,n2,n3,n4):
 
     return c
 
-u = suma(1,2,3,4,5)
+u = suma(1,2,3,4)
 print(u)
 
 
 def resta(b1,b2):
     if b1 > b2:
         y = b1 - b2
-        
-        return y
+    else:
+        return 0
 
-x = resta(4,2)
+capturadatosdefuncion = resta(6,1)
 
-print(x)
+if capturadatosdefuncion ==0:
+    print("No queremos mostrar numeros negativos")
+else:
+    print(capturadatosdefuncion)
+
+
+
+
+
 
 def division(e1,e2):
     f = e1 / e2
