8e08b136fe33e136b687fa0f5c44147cc98602ca
bayron74 <88299465+bayron74@users.noreply.github.com>
2021-10-09 10:20:29 -1200

    Create Ejercicios 09-10 2021.py
    primer commit

---
+++ Ejercicios 09-10 2021.py
@@ -0,0 +1,27 @@
+def suma(n1,n2,n3,n4,n5):
+    c = n1 + n2 + n3 + n4
+
+    return c
+
+u = suma(1,2,3,4,5)
+print(u)
+
+
+def resta(b1,b2):
+    if b1 > b2:
+        y = b1 - b2
+        
+        return y
+
+x = resta(4,2)
+
+print(x)
+
+def division(e1,e2):
+    f = e1 / e2
+    return f
+
+k = resta(5,4)
+
+print(k)
+    
